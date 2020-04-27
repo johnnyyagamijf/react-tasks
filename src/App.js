11d5b3b6804f06ee5cb39c66f4799093b49fecce
jmdc <johnmdcampos@gmail.com>
2020-04-27 13:22:55 -0300

    criaçao do componente main

--- src/App.js
+++ src/App.js
@@ -1,66 +1,15 @@
 import React, {Component} from 'react';
-import axios from 'axios';
 
 import Header from './components/Layout/Header';
-import Card from './components/Layout/Card'
+import Main from './components/Layout/Main'
 import './app.css';
-import api from '../src/services/api';
 
 export default class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      docs: [],
-      pages: 0,
-      page: 1
-    }
-  }
- async componentDidMount(){
-  this.getTasks();
-  }
-
-   getTasks = async () =>{
-    const response = await api.get(`/tasks?page=${this.state.page}`);
-    const {docs, pages} = response.data;
-    this.setState({docs, pages});
-  }
-
-   nextPage = async() => {
-    const nPage = this.state.page + 1;
-
-    if (nPage > this.state.pages){
-      return;
-    }
-    
-    await this.setState({page: nPage});
-    this.getTasks();
-  }
-
-  beforePage = async () => {
-    const nPage = this.state.page - 1;
-
-    if (nPage < 1){
-      return;
-    }
-
-   await this.setState({page: nPage});
-    this.getTasks();
-  }
   render() {
     return(
       <div className="App">
         <Header />
-        <div className="itens">
-        {
-          this.state.docs.map(task =>(
-            <Card title={task.title} description={task.description}/>
-          ))
-        }  
-        <div className="action">
-          <button onClick={this.nextPage}>Próxima</button>
-          <button onClick={this.beforePage}>Anterior</button>
-        </div>
-        </div>
+        <Main />
       </div>
     );
   }
---
+++ src/components/Layout/Main.js
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+import Header from './Header';
+import Card from './Card'
+import api from '../../services/api';
+
+export default class Main extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      docs: [],
+      pages: 0,
+      page: 1
+    }
+  }
+ async componentDidMount(){
+  this.getTasks();
+  }
+
+   getTasks = async () =>{
+    const response = await api.get(`/tasks?page=${this.state.page}`);
+    const {docs, pages} = response.data;
+    this.setState({docs, pages});
+  }
+
+   nextPage = async() => {
+    const nPage = this.state.page + 1;
+
+    if (nPage > this.state.pages){
+      return;
+    }
+    
+    await this.setState({page: nPage});
+    this.getTasks();
+  }
+
+  beforePage = async () => {
+    const nPage = this.state.page - 1;
+
+    if (nPage < 1){
+      return;
+    }
+
+   await this.setState({page: nPage});
+    this.getTasks();
+  }
+  render() {
+    return(
+        <div className="itens">
+        {
+          this.state.docs.map(task =>(
+            <Card title={task.title} description={task.description}/>
+          ))
+        }  
+        <div className="action">
+          <button onClick={this.nextPage}>Próxima</button>
+          <button onClick={this.beforePage}>Anterior</button>
+        </div>
+        </div>
+    );
+  }
+}
